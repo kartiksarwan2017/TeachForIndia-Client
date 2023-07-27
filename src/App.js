fd5549126e054390673da529a25a07d0e1b1981d
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-07-27 11:35:10 +0530

    Creating various Routes and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {Routes, Route} from "react-router-dom";
+import AdminRegister from "./components/AdminRegister/AdminRegister";
+import AdminLogin from "./components/AdminLogin/AdminLogin";
+import VolunteerLogin from "./components/VolunteerLogin/VolunteerLogin";
+import VolunteerRegister from "./components/VolunteerRegister/VolunteerRegister";
+import Home from "./components/Home/Home";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+     <Routes>
+      <Route exact path="/admin-register" element={<AdminRegister />} />
+      <Route exact path="/admin-login" element={<AdminLogin />} />
+      <Route exact path="/volunteer-register" element={<VolunteerLogin />} />
+      <Route exact path="/volunteer-login" element={<VolunteerRegister />} />
+      <Route exact path="/home" element={<Home />} />
+     </Routes>
     </div>
   );
 }
---
+++ src/components/AdminRegister/AdminRegister.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminRegister = () => {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
+
+export default AdminRegister;
