27e1b8fcbddf275611a26c5ceb3a5afcee3d40bb
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-07-28 20:06:15 +0530

    Updating footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Footer.css";
+import {Link} from "react-router-dom";
 
 
 const Footer = () => {
   return (
 <footer className="new_footer_area bg_color">
   <div className="new_footer_top">
-    <div className="container">
+    <div className="footercontainer">
       <div className="row">
         <div className="col-lg-3 col-md-6">
           <div className="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style={{visibility: "visible", animationDelay: "0.2s",  animationName: "fadeInLeft"}}>
@@ -24,12 +25,12 @@ const Footer = () => {
           <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" style={{visibility: "visible", animationDelay: "0.4s",  animationName: "fadeInLeft"}}>
             <h3 className="f-title f_600 t_color f_size_18">Download</h3>
             <ul className="list-unstyled f_list">
-              <li><a href="#">Company</a></li>
-              <li><a href="#">Android App</a></li>
-              <li><a href="#">ios App</a></li>
-              <li><a href="#">Desktop</a></li>
-              <li><a href="#">Projects</a></li>
-              <li><a href="#">My tasks</a></li>
+              <li><Link href="#">Company</Link></li>
+              <li><Link href="#">Android App</Link></li>
+              <li><Link href="#">ios App</Link></li>
+              <li><Link href="#">Desktop</Link></li>
+              <li><Link href="#">Projects</Link></li>
+              <li><Link href="#">My tasks</Link></li>
             </ul>
           </div>
         </div>
@@ -37,12 +38,12 @@ const Footer = () => {
           <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style={{visibility: "visible", animationDelay: "0.6s",  animationName: "fadeInLeft"}}>
             <h3 className="f-title f_600 t_color f_size_18">Help</h3>
             <ul className="list-unstyled f_list">
-              <li><a href="#">FAQ</a></li>
-              <li><a href="#">Term &amp; conditions</a></li>
-              <li><a href="#">Reporting</a></li>
-              <li><a href="#">Documentation</a></li>
-              <li><a href="#">Support Policy</a></li>
-              <li><a href="#">Privacy</a></li>
+              <li><Link href="#">FAQ</Link></li>
+              <li><Link href="#">Term &amp; conditions</Link></li>
+              <li><Link href="#">Reporting</Link></li>
+              <li><Link href="#">Documentation</Link></li>
+              <li><Link href="#">Support Policy</Link></li>
+              <li><Link href="#">Privacy</Link></li>
             </ul>
           </div>
         </div>
@@ -50,10 +51,10 @@ const Footer = () => {
           <div className="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style={{visibility: "visible", animationDelay: "0.8s",  animationName: "fadeInLeft"}}>
             <h3 className="f-title f_600 t_color f_size_18">Team Solutions</h3>
             <div className="f_social_icon">
-              <a href="#" className="fab fa-facebook"></a>
-              <a href="#" className="fab fa-twitter"></a>
-              <a href="#" className="fab fa-linkedin"></a>
-              <a href="#" className="fab fa-pinterest"></a>
+              <Link href="#" className="fab fa-facebook"></Link>
+              <Link href="#" className="fab fa-twitter"></Link>
+              <Link href="#" className="fab fa-linkedin"></Link>
+              <Link href="#" className="fab fa-pinterest"></Link>
             </div>
           </div>
         </div>
